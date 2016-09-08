6cd72cf5aa4c8581acb5994932f96599c777011c
Paluci <416063@CTS.COM>
2016-09-08 16:58:47 -0300

    Agregando annotations @Services y @Transactional a EventServiceImp

--- src/main/java/com/cognizant/ctscandidates/services/EventServiceImp.java
+++ src/main/java/com/cognizant/ctscandidates/services/EventServiceImp.java
@@ -1,5 +1,11 @@
 package com.cognizant.ctscandidates.services;
 
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class EventServiceImp implements EventService{
 
 	

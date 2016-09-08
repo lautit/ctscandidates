6efbda4d3885d090b7cc07a615cdb1cd17541e50
Paluci <416063@CTS.COM>
2016-09-08 16:50:52 -0300

    Agregando annotations @Repository a EventDAOImp

--- src/main/java/com/cognizant/ctscandidates/dao/EventDAOImp.java
+++ src/main/java/com/cognizant/ctscandidates/dao/EventDAOImp.java
@@ -1,7 +1,11 @@
 package com.cognizant.ctscandidates.dao;
 
+import org.springframework.stereotype.Repository;
+
 import com.cognizant.ctscandidates.bo.Event;
 
+
+@Repository
 public class EventDAOImp extends GenericDAOImp<Event, Long> implements EventDAO{
 
 	

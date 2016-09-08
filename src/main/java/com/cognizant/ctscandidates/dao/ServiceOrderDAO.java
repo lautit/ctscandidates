f42b4a8129a2db61cfa1e8a4681245573d49a5cd
Iudica <588477@CTS.COM>
2016-09-08 16:25:28 -0300

    ServiceOrderDAO

---
+++ src/main/java/com/cognizant/ctscandidates/dao/ServiceOrderDAO.java
@@ -0,0 +1,7 @@
+package com.cognizant.ctscandidates.dao;
+
+import com.cognizant.ctscandidates.bo.ServiceOrder;
+
+public interface ServiceOrderDAO extends GenericDAO<ServiceOrder, Long> {
+
+}
---
+++ src/main/java/com/cognizant/ctscandidates/dao/ServiceOrderDAOImp.java
@@ -0,0 +1,7 @@
+package com.cognizant.ctscandidates.dao;
+
+import com.cognizant.ctscandidates.bo.ServiceOrder;
+
+public class ServiceOrderDAOImp extends GenericDAOImp<ServiceOrder, Long> implements ServiceOrderDAO {
+
+}

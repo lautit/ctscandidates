2d16e81d844d111835aa1272538938872906b83a
Paluci <416063@CTS.COM>
2016-09-09 10:12:16 -0300

    Agregando metodo delete a clase TimelineServiceImp

--- src/main/java/com/cognizant/ctscandidates/services/TimelineServiceImp.java
+++ src/main/java/com/cognizant/ctscandidates/services/TimelineServiceImp.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cognizant.ctscandidates.bo.Timeline;
-import com.cognizant.ctscandidates.dao.EventDAO;
 import com.cognizant.ctscandidates.dao.TimelineDAO;
 
 @Service

400d2446edb84d1cf1a3cdd8bb346f2125134a6c
Lautaro Tejerina <lautit@gmail.com>
2016-09-09 15:56:09 -0300

    Levanta el index!

--- src/main/java/com/cognizant/ctscandidates/controllers/HomeController.java
+++ src/main/java/com/cognizant/ctscandidates/controllers/HomeController.java
@@ -5,10 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@RequestMapping(value = "/home")
 @Controller
 public class HomeController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index(Model model) {
  
 		return null;

ec2f36effe73f7a2f2f43a91624bf4630fe7b706
Iudica <588477@CTS.COM>
2016-09-09 10:21:09 -0300

    ServiceOrderService

--- src/main/java/com/cognizant/ctscandidates/services/ServiceOrderServiceImp.java
+++ src/main/java/com/cognizant/ctscandidates/services/ServiceOrderServiceImp.java
@@ -1,11 +1,15 @@
 package com.cognizant.ctscandidates.services;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cognizant.ctscandidates.bo.ServiceOrder;
 import com.cognizant.ctscandidates.dao.ServiceOrderDAO;
+
 
 @Service
 @Transactional
@@ -16,6 +20,41 @@ public class ServiceOrderServiceImp implements ServiceOrderService {
 	
 	@Autowired
 	private ServiceOrderDAO serviceOrderDAO;
+	
+	@Override 
+	public Long save(ServiceOrder serviceOrder) {
+		return serviceOrderDAO.save(serviceOrder);
+	}
+	
+	@Override 
+	public void update(ServiceOrder serviceOrder){
+		serviceOrderDAO.update(serviceOrder);
+	}
+	
+	@Override
+	public void delete(Long id) {
+		serviceOrderDAO.delete(id);
+	}
+	
+	@Override
+	public ServiceOrder searchById(Long id) {
+		return serviceOrderDAO.searchById(id);
+	}
+	
+	@Override
+	public List<ServiceOrder> getAll() {
+		return serviceOrderDAO.getAll();
+	}
+	
+	@Override
+	public List<ServiceOrder> getAll(int page, int rows) {
+		return serviceOrderDAO.getAll(page, rows);
+	}
+	
+	@Override
+	public Long count() {
+		return serviceOrderDAO.count();
+	}
 	
 	
 }

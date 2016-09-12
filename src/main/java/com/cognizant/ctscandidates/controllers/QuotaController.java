808456e8362d870415da864867d6fe0cee66e9e0
Paluci <416063@CTS.COM>
2016-09-12 17:55:23 -0300

    agregando metodo details que captura id de quote

--- src/main/java/com/cognizant/ctscandidates/controllers/QuotaController.java
+++ src/main/java/com/cognizant/ctscandidates/controllers/QuotaController.java
@@ -1,9 +1,15 @@
 package com.cognizant.ctscandidates.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.cognizant.ctscandidates.bo.Quota;
 import com.cognizant.ctscandidates.services.QuotaService;
 
 @RequestMapping("/quota")
@@ -13,6 +19,13 @@ public class QuotaController {
 	@Autowired
 	QuotaService quotaService;
 	
-
+	
+	@RequestMapping(value = "/details", method = RequestMethod.GET)
+	public String details(@RequestParam Long id, Model model){
+			
+			Quota quota = quotaService.searchById(id);
+			model.addAttribute("quota", quota);	
+		return null;
+	}
 
 }

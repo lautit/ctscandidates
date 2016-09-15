e1f9f129f6f126f8bd0ffdf1d01e355b4d9a2f7b
Paluci <416063@CTS.COM>
2016-09-15 10:51:15 -0300

    error de index en lista timeline reparado

--- src/main/java/com/cognizant/ctscandidates/controllers/QuotaController.java
+++ src/main/java/com/cognizant/ctscandidates/controllers/QuotaController.java
@@ -33,8 +33,7 @@ public class QuotaController {
 	public String details(@RequestParam Long id, Model model){
 		
 		Quota quota = quotaService.searchById(id);
-<<<<<<< HEAD
-		
+
 		/*TESTING PURPOSES WITHOUT DB
 		//we get the events:
 		Event event1 = eventService.searchById(1L);
@@ -90,56 +89,7 @@ public class QuotaController {
 			model.addAttribute("actualEvents", actualEvents);	
 			
 			
-=======
 
-		/*
-		 * FOR TESTING PURPOSES WITHOUT BD //we get the events: Event event1 =
-		 * eventService.searchById(1L); Event event2 =
-		 * eventService.searchById(2L); Event event3 =
-		 * eventService.searchById(3L); Event event4 =
-		 * eventService.searchById(4L); //we get the timelines: Timeline
-		 * timeline1 = timelineService.searchById(1L); Timeline timeline2 =
-		 * timelineService.searchById(2L);
-		 * 
-		 * 
-		 * timeline1.getEvents().add(event1); timeline1.getEvents().add(event2);
-		 * 
-		 * timeline2.getEvents().add(event3); timeline2.getEvents().add(event4);
-		 * 
-		 * quota.getTimelineList().add(timeline1);
-		 * quota.getTimelineList().add(timeline2);
-		 * 
-		 * END TEST
-		 */
-
-		// get latest event name based on event date in a list
-		List<Double> eventPercentage = new ArrayList<Double>();
-		List<Event> actualEvents = new ArrayList<Event>();
-
-		for (Timeline timeline : quota.getTimelineList()) {
-
-			Event lastEvent = new Event();
-
-			// we get the last event in the timeline
-			if((timeline.getEvents().size() < 0)){
-			lastEvent = timeline.getEvents().get(timeline.getEvents().size());
-			}else{
-				lastEvent = timeline.getEvents().get(timeline.getEvents().size() - 1);
-			}
-			// we add the last event to an event list
-			actualEvents.add(lastEvent);
-			// then change the event name to percentage and add it to a list
-			eventPercentage.add(eventService.convertNameToDouble(lastEvent.getName()));
-
-		}
-
-		quotaService.save(quota);
-
-		model.addAttribute("quota", quota);
-		model.addAttribute("eventPercentage", eventPercentage);
-		model.addAttribute("actualEvents", actualEvents);
-
->>>>>>> 31ddae90b5d06221b7c32f65f53d019ceeb17292
 		return null;
 	}
 
